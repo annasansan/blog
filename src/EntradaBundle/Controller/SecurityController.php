ec6485c610c72703e5c1a6aedafdd8ab38c79bae
Anna Sansano <asansano@howdeniberia.com>
2017-12-20 11:58:33 +0100

    tema 8

---
+++ src/EntradaBundle/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+namespace EntradaBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+class SecurityController extends Controller
+{
+    /**
+     * @Template()
+     */
+    
+    public function loginAction(Request $request){
+        
+        $authenticationUtils = $this->get('security.authentication_utils');
+        
+        $error = $authenticationUtils->getLastAuthenticationError();
+        
+        $lastUsername = $authenticationUtils->getLastUsername();
+        
+        return array(
+            'last_username' =>  $lastUsername,
+            'error' =>  $error
+        );
+        
+    }
+}
+
+
